24d2dc6b80505d22c6678cc5eeeadf511e107e79
Dodowxoja <xoja2001mk@gamil.com>
2022-06-15 19:17:54 +0500

    add

--- lib/mario_animation.dart
+++ lib/mario_animation.dart
@@ -10,6 +10,7 @@ class MarioAnimation extends StatefulWidget {
 class _MarioAnimationState extends State<MarioAnimation>
     with TickerProviderStateMixin {
   int ball = 0;
+  bool gameOver = false;
 
   AnimationController? animationControllerMario;
   AnimationController? animationControllerWall;
@@ -28,6 +29,7 @@ class _MarioAnimationState extends State<MarioAnimation>
     animationControllerMario!.forward();
 
     animationControllerMario!.addListener(() {
+      gameOverFunc();
       setState(() {});
     });
 
@@ -47,6 +49,7 @@ class _MarioAnimationState extends State<MarioAnimation>
     animationControllerWall!.forward();
 
     animationControllerWall!.addListener(() {
+      gameOverFunc();
       setState(() {});
     });
 
@@ -55,6 +58,59 @@ class _MarioAnimationState extends State<MarioAnimation>
         animationControllerWall!.repeat();
       }
     });
+    ballMario();
+  }
+
+  ballMario() async {
+    for (var i = 0; i <= ball; i++) {
+      if (gameOver == false) {
+        await Future.delayed(const Duration(milliseconds: 200), () {
+          ball += 1;
+          setState(() {});
+        });
+      } else {
+        break;
+      }
+    }
+  }
+
+  gameOverFunc() {
+    if (animationControllerWall!.value.toInt() >= 270) {
+      if (animationControllerWall!.value.toInt() <= 350) {
+        if (animationControllerMario!.value.toInt() <= 70) {
+          animationControllerMario!.dispose();
+          animationControllerWall!.dispose();
+          gameOver = true;
+          showDialog(
+              context: context,
+              builder: (_) {
+                return AlertDialog(
+                  title: Column(
+                    children: [
+                      const Text('GAME OVER'),
+                      IconButton(
+                        color: Colors.green,
+                        icon: const Icon(
+                          Icons.replay,
+                          size: 30,
+                        ),
+                        onPressed: () {
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                builder: (_) => const MarioAnimation(),
+                              ),
+                              (route) => false);
+                        },
+                      )
+                    ],
+                  ),
+                );
+              });
+          setState(() {});
+        }
+      }
+    }
   }
 
   @override
@@ -81,7 +137,6 @@ class _MarioAnimationState extends State<MarioAnimation>
                         height: 100,
                       ),
                     ),
-
                     //WALL ANIMATION
                     Positioned(
                       right: animationControllerWall!.value,
